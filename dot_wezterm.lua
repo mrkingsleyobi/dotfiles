2da7fee359e939bdd5eaf5d3e25173979997444f
Kingsley Obi <mofuobi@gmail.com>
2026-07-20 17:11:07 +0100

    Update .wezterm.lua

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -330,6 +330,6 @@ config.key_tables = {
 config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = false
 config.hide_tab_bar_if_only_one_tab = false
-config.tab_and_split_indices_are_zero_based = true
+config.tab_and_split_indices_are_zero_based = false
 
 return config
